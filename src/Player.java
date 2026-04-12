6c5999ddbc83307c489a4d40e587d109916d036e
Maksym Kulynych <kulinichmax16082009@gmail.com>
2026-04-12 12:26:39 +0200

    Parameters were added to plater class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,37 @@
 public class Player {
+    private int score;
+    private long time;
+
+    public Player() {
+        this.score = 0;
+        this.time = 0;
+    }
+
+    public void addScore(int points) {
+        this.score += points;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "score=" + score +
+                ", time=" + time +
+                '}';
+    }
 }

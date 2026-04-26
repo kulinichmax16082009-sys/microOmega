be1f51297664115c74ec8cd21c28d607cd03712f
Maksym Kulynych <kulinichmax16082009@gmail.com>
2026-04-26 19:59:08 +0200

    Game window was initialized. Constructor and initialization of label were added.

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,5 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class GameWindow extends BasicWindow {
+
+    private JLabel scoreLabel;
+    private final GamePanel gamePanel;
+
+    public final static int CELL_SIZE = 100;
+    public final static int CELLS_COUNT = 4;
+    public final static int GAP_SIZE = 12;
+
     public GameWindow(BasicWindow nextWindow) {
-        super("", 400, nextWindow);
+        super("2048 - Game", CELL_SIZE * CELLS_COUNT + GAP_SIZE * (CELLS_COUNT + 1), nextWindow);
+        gamePanel = new GamePanel();
+        gamePanel.setPreferredSize(new Dimension(windowSize, windowSize));
+
+        intiScoreLabel();
+
+        frame.add(scoreLabel, BorderLayout.NORTH);
+        frame.add(gamePanel);
+
+        //Final settings
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+    }
+
+    public void intiScoreLabel() {
+        scoreLabel = new JLabel("Score: 0 | Time: 0 s", JLabel.CENTER);
+        scoreLabel.setOpaque(true);
+        scoreLabel.setBackground(new Color(195, 196, 195));
+    }
+
+    public void updateLabel(int score, long time) {
+        scoreLabel.setText("Score: " + score + " | Time: " + time + " s");
     }
 }

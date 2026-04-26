66d38b989dbc6072df1dca64ff3b9759352f9521
Maksym Kulynych <kulinichmax16082009@gmail.com>
2026-04-26 11:57:07 +0200

    BasicWindow class was created as parent of all window classes

---
+++ src/BasicWindow.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BasicWindow {
+    protected JFrame frame;
+    protected BasicWindow nextWindow;
+    protected int windowSize;
+
+    public BasicWindow(String title, int windowSize, BasicWindow nextWindow) {
+        this.windowSize = windowSize;
+        this.nextWindow = nextWindow;
+
+        frame = new JFrame(title);
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(false);
+    }
+
+    public void show() {
+        frame.setVisible(true);
+    }
+
+    public void hide() {
+        frame.setVisible(false);
+    }
+
+    public void showNextWindow() {
+        hide();
+        nextWindow.show();
+    }
+}

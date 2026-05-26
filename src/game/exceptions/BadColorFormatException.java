72908cbcfa12e421568aa730dea12e02743214ff
Maksym Kulynych <kulinichmax16082009@gmail.com>
2026-05-26 17:59:53 +0200

    Exception documentation was added.

--- src/game/exceptions/BadColorFormatException.java
+++ src/game/exceptions/BadColorFormatException.java
@@ -1,5 +1,10 @@
 package game.exceptions;
 
+/**
+ * This exception is thrown when the color format in the .json file is incorrect.
+ *
+ * @author Maksym Kulynych
+ */
 public class BadColorFormatException extends RuntimeException {
     public BadColorFormatException() {
         super();
